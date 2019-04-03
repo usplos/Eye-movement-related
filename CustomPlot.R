39f6c8a5a1f6d26059ed3e45de2005277e000dce
Yaw <zgy070620@163.com>
2019-04-03 18:37:35 +0800

    Update CustomPlot.R

--- CustomPlot.R
+++ CustomPlot.R
@@ -100,3 +100,102 @@ InteractionplotBiV = function(df, IndeName, DeName,Dot = F, Alpha = 0.2, Color =
   
   print(p);return(p)
 }
+
+DemoTriV = read_csv('DemoCustomPlotTriV.csv')
+DemoTriV = within(DemoTriV,{
+  CondA = factor(CondA)
+  CondB = factor(CondB)
+  CondC = factor(CondC)
+})
+
+BarplotTriV = function(df, IndeName, DeName,Fill = c('red','blue'), Colnum = 2, Dot = F, Alpha = 0.2, Color = 'black', Dodge = 0.5){
+  if(!require(tidyverse)){install.packages('tidyverse')}
+  library(tidyverse)
+  IndeName1 = IndeName[[1]];IndeName2 = IndeName[[2]];IndeName3 = IndeName[[3]]
+  
+  dfTidy = df %>% group_by(eval(parse(text = IndeName1)),
+                           eval(parse(text = IndeName2)),
+                           eval(parse(text = IndeName3))) %>% summarise(M = mean(eval(parse(text = DeName))),
+                                                                        SE = sd(eval(parse(text = DeName)))/sqrt(length(eval(parse(text = DeName)))))
+  names(dfTidy)[1:3] = IndeName
+  
+  p = ggplot(data = dfTidy,
+             aes(x = eval(parse(text = IndeName1)),
+                 y = M,
+                 fill = eval(parse(text = IndeName2))))+
+    geom_bar(position = position_dodge(Dodge),
+             stat = 'identity',
+             width = 0.5)+
+    geom_errorbar(aes(ymin = M-SE,
+                      ymax = M+SE),
+                  width = 0.05,
+                  position = position_dodge(Dodge))+
+    facet_wrap(~eval(parse(text = IndeName3)), ncol = Colnum)+
+    labs(y = DeName, x = IndeName1, fill = IndeName2)+
+    scale_fill_manual(values = Fill)
+  
+  if(isTRUE(Dot)){
+    p = p + geom_point(data = df %>% 
+                         filter(eval(parse(text = IndeName1)) %in% levels(eval(parse(text = IndeName1)))[[1]]) %>%
+                         mutate(xp = ifelse(eval(parse(text = IndeName2)) %in% levels(eval(parse(text = IndeName2)))[[1]], 0.875, 1.125)), 
+                       aes(x = xp, y = eval(parse(text = DeName))),
+                       alpha = Alpha,
+                       color = Color,
+                       show.legend = F) +
+      geom_point(data = df %>% 
+                   filter(eval(parse(text = IndeName1)) %in% levels(eval(parse(text = IndeName1)))[[2]]) %>%
+                   mutate(xp = ifelse(eval(parse(text = IndeName2)) %in% levels(eval(parse(text = IndeName2)))[[1]], 1.125, 2.125)), 
+                 aes(x = xp, y = eval(parse(text = DeName))),
+                 alpha = Alpha,
+                 color = Color,
+                 show.legend = F)
+  }
+  
+  print(p);return(p)
+}
+
+InteractionplotTriV = function(df, IndeName, DeName, Color = c('red','blue'), Dot = F, Colnum = 2, Alpha = 0.2, Dodge = 0.5, PColor = 'black'){
+  if(!require(tidyverse)){install.packages('tidyverse')}
+  library(tidyverse)
+  IndeName1 = IndeName[[1]];IndeName2 = IndeName[[2]];IndeName3 = IndeName[[3]]
+  
+  dfTidy = df %>% group_by(eval(parse(text = IndeName1)),
+                           eval(parse(text = IndeName2)),
+                           eval(parse(text = IndeName3))) %>% summarise(M = mean(eval(parse(text = DeName))),
+                                                                        SE = sd(eval(parse(text = DeName)))/sqrt(length(eval(parse(text = DeName)))))
+  names(dfTidy)[1:3] = IndeName
+  
+  p = ggplot(data = dfTidy,
+             aes(x = eval(parse(text = IndeName1)),
+                 y = M,
+                 color = eval(parse(text = IndeName2))))+
+    geom_point(position = position_dodge(Dodge))+
+    geom_errorbar(aes(ymin = M-SE,
+                      ymax = M+SE),
+                  width = 0.05,
+                  position = position_dodge(Dodge))+
+    geom_line(aes(group = eval(parse(text = IndeName2))),
+              position = position_dodge(Dodge))+
+    facet_wrap(~eval(parse(text = IndeName3)), ncol = Colnum)+
+    labs(x = IndeName1, y = DeName, color = IndeName2)+
+    scale_colour_manual(values = Color)
+  
+  if(isTRUE(Dot)){
+    p = p + geom_point(data = df %>% 
+                         filter(eval(parse(text = IndeName1)) %in% levels(eval(parse(text = IndeName1)))[[1]]) %>%
+                         mutate(xp = ifelse(eval(parse(text = IndeName2)) %in% levels(eval(parse(text = IndeName2)))[[1]], 0.875, 1.125)), 
+                       aes(x = xp, y = eval(parse(text = DeName))),
+                       alpha = Alpha,
+                       color = PColor,
+                       show.legend = F) +
+      geom_point(data = df %>% 
+                   filter(eval(parse(text = IndeName1)) %in% levels(eval(parse(text = IndeName1)))[[2]]) %>%
+                   mutate(xp = ifelse(eval(parse(text = IndeName2)) %in% levels(eval(parse(text = IndeName2)))[[1]], 1.125, 2.125)), 
+                 aes(x = xp, y = eval(parse(text = DeName))),
+                 alpha = Alpha,
+                 color = PColor,
+                 show.legend = F)
+  }
+  
+  print(p);return(p)
+}
