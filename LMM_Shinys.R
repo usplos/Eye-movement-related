d21a82c2e03e5cb66c5269b13c3f7b89b58a3212
Yaw <zgy070620@163.com>
2019-09-13 11:14:56 +0800

    Update LMM_Shinys.R

--- LMM_Shinys.R
+++ LMM_Shinys.R
@@ -139,7 +139,7 @@ if(as.double(R.Version()$minor) >= 6.1){
         MConverge = ifelse(length(M@optinfo$conv$lme4$messages[grep(pattern = 'Model failed to converge',
                                                                     x = M@optinfo$conv$lme4$message)]) > 0,
                            F,T)
-        M.Singular = isSingular(M)
+        M.Singular = isSingular(M, tol = 0.0001)
         try({
           R2.C = MuMIn::r.squaredGLMM(M)[[2]]
           R2.M = MuMIn::r.squaredGLMM(M)[[1]]
@@ -157,7 +157,7 @@ if(as.double(R.Version()$minor) >= 6.1){
         M = glmer(data = df, as.formula(Formulas[formula.id]), family = Family)
         MAIC = AIC(M)
         MBIC = BIC(M)
-        M.Singular = isSingular(M)
+        M.Singular = isSingular(M, tol = 0.0001)
         MConverge = ifelse(length(M@optinfo$conv$lme4$messages[grep(pattern = 'Model failed to converge',
                                                                     x = M@optinfo$conv$lme4$message)]) > 0,
                            F,T)
@@ -200,7 +200,7 @@ if(as.double(R.Version()$minor) >= 6.1){
       M = lmer(data = df, Formula)
       MAIC = AIC(M)
       MBIC = BIC(M)
-      M.Singular = isSingular(M)
+      M.Singular = isSingular(M, tol = 0.0001)
       resulttable = data.frame(formula = Formula,
                                AIC = MAIC,
                                BIC = MBIC,
@@ -210,7 +210,7 @@ if(as.double(R.Version()$minor) >= 6.1){
       M = glmer(data = df, Formula, family = Family)
       MAIC = AIC(M)
       MBIC = BIC(M)
-      M.Singular = isSingular(M)
+      M.Singular = isSingular(M, tol = 0.0001)
       resulttable = data.frame(formula = Formula,
                                AIC = MAIC,
                                BIC = MBIC,
