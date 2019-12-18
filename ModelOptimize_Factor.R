5f75e994e561c1b8c6192dbb16361eb7678e3b70
Yaw <zgy070620@163.com>
2019-12-18 16:02:41 +0800

    Update ModelOptimize_Factor.R

--- ModelOptimize_Factor.R
+++ ModelOptimize_Factor.R
@@ -1,6 +1,21 @@
 
 # 创建contr.simple()函数
-source('https://raw.githubusercontent.com/usplos/Eye-movement-related/master/ModelOptimize_Factor.R')
+source('https://raw.githubusercontent.com/usplos/Eye-movement-related/master/contr.simple.R')
+
+# 创建生成虚拟变量的函数
+ModelMatrix = function(Data, Fix_Factor, MatrixDesign = '*'){
+  Data[Fix_Factor] = lapply(Data[Fix_Factor], factor)
+  for(ff in Fix_Factor){
+    contrasts(Data[[ff]]) = contr.simple(length(levels(Data[[ff]])))
+  }
+  
+  eval(parse(text = paste0('mmff = model.matrix(~ ',paste0(Fix_Factor,collapse = MatrixDesign),', Data)')))
+  IVName = gsub(pattern = ':',replacement = '_',x = colnames(mmff)[2:ncol(mmff)]) %>%
+    substr(x = ., start = 1, stop = nchar(.))
+  Data[IVName] = mmff[,2:ncol(mmff)]
+  
+  return(Data)
+}
 
 # 创建ModelOptimize_Factor()函数
 ModelOptimize_Factor = function(FormulaManual = NULL,Data, DV, Fix_Factor, Re_Factor,
